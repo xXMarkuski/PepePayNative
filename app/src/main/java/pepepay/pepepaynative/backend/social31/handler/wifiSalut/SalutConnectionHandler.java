2eff9025ee585b3768963d8e3d443a0c7af5e07c
romano21A <romano21A@users.noreply.github.com>
2016-12-11 16:22:20 +0100

    First work at implementation of the Salut library

---
+++ app/src/main/java/pepepay/pepepaynative/backend/social31/handler/wifiSalut/SalutConnectionHandler.java
@@ -0,0 +1,108 @@
+package pepepay.pepepaynative.backend.social31.handler.wifiSalut;
+
+import android.app.Activity;
+import android.os.Build;
+
+import com.peak.salut.*;
+import com.peak.salut.Callbacks.SalutDataCallback;
+import com.peak.salut.Callbacks.SalutDeviceCallback;
+
+import pepepay.pepepaynative.backend.social31.ConnectionManager;
+import pepepay.pepepaynative.backend.social31.handler.IDevice;
+import pepepay.pepepaynative.backend.social31.handler.IDeviceConnectionHandler;
+import pepepay.pepepaynative.utils.Function;
+
+
+
+public class SalutConnectionHandler implements IDeviceConnectionHandler<SalutConnectionHandler, WifiSalutDevice>, SalutDataCallback {
+
+    public static final String TAG = "salutconnectionhandler";
+
+    public static final String SERVICE_NAME = "pepepay";
+
+    public SalutDataReceiver dataReceiver;
+    public SalutServiceData serviceData;
+    public Salut network;
+    public SalutDataCallback callback;
+
+    private final Activity activity;
+    private ConnectionManager connManager;
+
+
+
+    public SalutConnectionHandler(Activity activity) {
+        this.activity = activity;
+    }
+
+
+    @Override
+    public boolean canInit() {
+        return true;
+    }
+
+    @Override
+    public void preInit(ConnectionManager manager) {
+        connManager = manager;
+    }
+
+    @Override
+    public void init(ConnectionManager manager) {
+
+        //TODO: change instanceName to user defined string
+        dataReceiver = new SalutDataReceiver(activity, this);
+        serviceData = new SalutServiceData(SERVICE_NAME, 60606, Build.MODEL);
+        network = new Salut(dataReceiver, serviceData, null);
+
+        network.startNetworkService(new SalutDeviceCallback() {
+            @Override
+            public void call(SalutDevice device) {
+                connManager.incomingConnection(new WifiSalutDevice(device), this);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void connect(WifiSalutDevice target) {
+
+    }
+
+    @Override
+    public void disconnect(WifiSalutDevice target) {
+
+    }
+
+    @Override
+    public void send(WifiSalutDevice target, String data) {
+
+    }
+
+    @Override
+    public boolean canSend() {
+        return false;
+    }
+
+    @Override
+    public Class getIDeviceType() {
+        return null;
+    }
+
+    @Override
+    public void requestAvailableDevices(Function callback) {
+
+    }
+
+    @Override
+    public void onDataReceived(Object o) {
+
+    }
+}
---
+++ app/src/main/java/pepepay/pepepaynative/backend/social31/handler/wifiSalut/WifiSalutDevice.java
@@ -0,0 +1,25 @@
+package pepepay.pepepaynative.backend.social31.handler.wifiSalut;
+
+import com.peak.salut.SalutDevice;
+
+import pepepay.pepepaynative.backend.social31.handler.IDevice;
+
+
+public class WifiSalutDevice implements IDevice<SalutConnectionHandler> {
+
+    private final SalutDevice salutDevice;
+
+    WifiSalutDevice(SalutDevice salutDevice) {
+        this.salutDevice = salutDevice;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return salutDevice.readableName;
+    }
+}
